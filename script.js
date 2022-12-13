46839f280826e3cd82647db149000e5db83c1f04
cristhian bustamante <cristhian_georgia_20@hotmail.com>
2022-12-12 19:40:55 -0500

    images-carousel

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
   let i;
-  let slides = document.getElementsByClassName("mySlides-fade");
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }}
-  slideIndex = 0;
-  let slides = document.getElementsByClassName("mySlides-fade");
+    slides[i].style.display = "none";  
+  }
   slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 
 const apiKey = '2259296caamsha3a762d7515a724p19faecjsne57da4f0abd4';
 
